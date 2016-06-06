2df829bf2f873d3a2829b755546665237624e61c
Javii <Javii@Javi>
2016-06-06 08:59:19 +0200

    Hacemos renames a todas las variables creadas para cargar con la solucion a la suma de los metodos de las otras clases respectivamente

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -6,15 +6,15 @@ public class Prin {
 
     public static void main(String[] args) throws IOException {
         Principal ObjDobleVariable = new Principal(10, 20);
-        int n = ObjDobleVariable.i;
-        System.out.println("Es: " + n);
+        int resultado1 = ObjDobleVariable.i;
+        System.out.println("Es: " + resultado1);
         
         Principal ObjVariable = new Principal(10);
-        int m = ObjVariable.i;
-        System.out.println("Es: " + m);
+        int resultado2 = ObjVariable.i;
+        System.out.println("Es: " + resultado2);
         
         Principal ObjSinVariable = new Principal();
-        int nn = ObjSinVariable.i;
-        System.out.println("Es: " + nn);
+        int resultado3 = ObjSinVariable.i;
+        System.out.println("Es: " + resultado3);
     }
 }
