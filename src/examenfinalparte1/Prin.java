4a6380545955457f9dc2798f1433e0df66762ebf
Usuario <Usuario@192.168.0.158>
2016-06-18 21:33:34 +0200

    Corregido todo el javadoc y refactor en ambas clases.

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -5,23 +5,20 @@ import java.io.IOException;
 public class Prin {
 
     /**
-     * Clase Main que crea 3 objetos de la calase "Principal" ejecutando en cada uno su respectivo constructor.
-     * Muestra por consola el resultado de las sumas creadas con los contrsuctores y el metodo met de la clase "Principal".
-     * Se dan los valores a las variables j y m para cada constructor
-     * @param args
+     * Este programa suma los valores dados en los constructores y muestra por pantalla el resultado
      * @throws IOException 
      */
     public static void main(String[] args) throws IOException {
         Principal ObjDobleVariable = new Principal(10, 20);
-        int resultado1 = ObjDobleVariable.i;
+        int resultado1 = ObjDobleVariable.solucion; //=30 (10+20)
         System.out.println("Es: " + resultado1);
         
         Principal ObjVariable = new Principal(10);
-        int resultado2 = ObjVariable.i;
+        int resultado2 = ObjVariable.solucion; //=20 (dos veces 10)
         System.out.println("Es: " + resultado2);
         
         Principal ObjSinVariable = new Principal();
-        int resultado3 = ObjSinVariable.i;
+        int resultado3 = ObjSinVariable.solucion; //=-1
         System.out.println("Es: " + resultado3);
     }
 }
